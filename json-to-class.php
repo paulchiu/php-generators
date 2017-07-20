bf021a3c4c9446d6d1de75468d4ada8df0478aec
Paul Chiu <paulchiu@gmail.com>
2017-07-21 01:59:09 +1000

    Refactored out reusable functions.

--- json-to-class.php
+++ json-to-class.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__.'/lib/strings.php';
+
 $className = 'AccessToken';
 $json = <<<JS
 {
@@ -23,56 +25,6 @@ $propertiesTemplate = <<<'PT'
     /** @var %hintType% $%variableName% */
     protected $%variableName%;
 PT;
-
-function snakeToPascal($string) {
-    return str_replace('_', '', ucwords($string, '_'));
-}
-
-function snakeToCamel($string) {
-    return lcfirst(snakeToPascal($string));
-}
-
-function pluralize($string) {
-    $lastChar = strtolower($string[strlen($string)-1]);
-
-    if ($lastChar === 's') {
-        return $string;
-    }
-
-    switch ($lastChar) {
-        case 'y':
-            return substr($string,0,-1).'ies';
-        case 's':
-            return $string.'es';
-        default:
-            return $string.'s';
-    }
-}
-
-function determineTypeVariables($value, $key = null) {
-    $hintType = $type = 'string';
-    $getVerb = 'get';
-    $quantNoun = $key;
-
-    if (is_integer($value)) {
-        $hintType = $type = 'int';
-    } elseif (is_float($value)) {
-        $hintType = $type = 'float';
-    } elseif (is_bool($value)) {
-        $hintType = $type = 'bool';
-        $getVerb = 'is';
-    } elseif (is_string($value) && strpos($value, ',') !== false) {
-        $arrayValues = array_map('trim', explode(',', $value));
-        $arrayValueType = determineTypeVariables($arrayValues[0])[0];
-        $hintType = sprintf('array|%s[]', $arrayValueType);
-        $type = 'array';
-        $quantNoun = pluralize($key);
-    } elseif (is_array($value)) {
-        $hintType = $type = snakeToPascal($key);
-    }
-
-    return [$hintType, $type, $getVerb, $quantNoun];
-}
 
 $getterSetterTemplate = <<<'GST'
     /**
---
+++ lib/strings.php
@@ -0,0 +1,52 @@
+<?php
+
+function snakeToPascal($string) {
+    return str_replace('_', '', ucwords($string, '_'));
+}
+
+function snakeToCamel($string) {
+    return lcfirst(snakeToPascal($string));
+}
+
+function pluralize($string) {
+    $lastChar = strtolower($string[strlen($string)-1]);
+
+    if ($lastChar === 's') {
+        return $string;
+    }
+
+    switch ($lastChar) {
+        case 'y':
+            return substr($string,0,-1).'ies';
+        case 's':
+            return $string.'es';
+        default:
+            return $string.'s';
+    }
+}
+
+function determineTypeVariables($value, $key = null) {
+    $hintType = $type = 'string';
+    $getVerb = 'get';
+    $quantNoun = $key;
+
+    if (is_integer($value)) {
+        $hintType = $type = 'int';
+    } elseif (is_float($value)) {
+        $hintType = $type = 'float';
+    } elseif (is_bool($value)) {
+        $hintType = $type = 'bool';
+        $getVerb = 'is';
+    } elseif (is_string($value) && strpos($value, ',') !== false) {
+        $arrayValues = array_map('trim', explode(',', $value));
+        $arrayValueType = determineTypeVariables($arrayValues[0])[0];
+        $hintType = sprintf('array|%s[]', $arrayValueType);
+        $type = 'array';
+        $quantNoun = pluralize($key);
+    } elseif (is_array($value)) {
+        $hintType = $type = snakeToPascal($key);
+    }
+
+    return [$hintType, $type, $getVerb, $quantNoun];
+}
+
